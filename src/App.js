5787174db0b15604fc83201994a891d73187b93a
Owin <lichaozheng.g@163.com>
2021-01-21 11:07:08 +0800

    05-右侧TabList开发完毕

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import FileSearch from './components/FileSearch'
 import FileList from './components/FileList'
 import defaultFiles from './utils/defaultFiles'
 import BottomBtn from './components/BottomBtn'
+import TabList from './components/TabList'
 
 function App() {
   return (
     <div className="App container-fluid px-0">
       <div className="row no-gutters">
-        <div className="col-6 bg-danger left-panel">
+        <div className="col-3 bg-danger left-panel">
           <FileSearch
             title="我的云文档"
             onFileSearch={ (value) => { console.log(value) } }
@@ -42,8 +43,15 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="col-6 bg-primary right-panel">
-          <h1>This is the right!</h1>
+        <div className="col-9 right-panel">
+          <TabList
+            files={defaultFiles}
+            activeId="1"
+            unsaveIds={["1", "2"]}
+            onTabClick={(id) => {console.log(id)}}
+            onCloseTab={(id) => {console.log('closing', id)}}
+          >
+          </TabList>
         </div>
       </div>
     </div>
--- src/utils/defaultFiles.js
+++ src/utils/defaultFiles.js
@@ -10,6 +10,12 @@ const defaultFiles = [
     title: 'second post',
     body: '## this is the title',
     createdAt: 2315786970
+  },
+  {
+    id: '3',
+    title: '你好世界',
+    body: '## this is the title',
+    createdAt: 2315786988
   }
 ]
 
